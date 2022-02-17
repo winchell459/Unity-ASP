7d2c2d012d0d364bfdea8156ae96c8cdb7f34792
winchell459 <johncmorris@gmail.com>
2022-02-17 14:37:26 -0800

    ASPTileRules fixed

--- Assets/Scripts/ASPGenerator/ASPTileRules.cs
+++ Assets/Scripts/ASPGenerator/ASPTileRules.cs
@@ -10,16 +10,54 @@ public class ASPTileRules : ScriptableObject
     {
         public string name;
         public Sprite sprite;
-        public bool[] neighbors;
+        public States[] neighbors;
     }
 
     public ASPTile[] Tiles;
 
-    public string GetTileRules()
+    public enum States
+    {
+        either,
+        filled,
+        empty
+    }
+
+    public virtual string GetTileRules()
     {
         string tile_rules = "";
         //generate tiles rules here
 
         return tile_rules;
+    }
+
+    protected List<bool[]> getMissingTiles(ASPTile[] tileRules)
+    {
+        List<bool[]> missingTiles = new List<bool[]>();
+        for(int i = 0; i < 256; i += 1)
+        {
+            bool[] permutation = new bool[8];
+            int num = i;
+            for(int j = 7; j >= 0; j -= 1)
+            {
+                int placeValue = num / (int)Mathf.Pow(2, j);
+                if (placeValue == 1) permutation[j] = true;
+                num = num % (int)Mathf.Pow(2, j);
+            }
+
+            bool missing = true;
+            foreach(ASPTile tileRule in tileRules)
+            {
+                bool found = true;
+                for(int j = 0; j < 8; j += 1)
+                {
+                    if (tileRule.neighbors[j] != States.either && permutation[j] != (tileRule.neighbors[j] == States.filled)) found = false;
+                }
+                if (found) missing = false;
+            }
+            if (missing) missingTiles.Add(permutation);
+        }
+
+
+        return missingTiles;
     }
 }
